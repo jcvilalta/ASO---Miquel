48a3c9e98a33336789e74808e6e012030b1a65ec
jcvilalta <jcastanyervilalta@gmail.com>
2024-11-21 19:32:07 +0100

    comprovacio errors i sg (20/11 i 21/11)

--- AWSCLI/scripts/crearSG.sh
+++ AWSCLI/scripts/crearSG.sh
@@ -6,6 +6,34 @@ aws ec2 create-security-group --group-name sg-hackaton --description "Security g
 # Obtenim el codi de sortida
 CODIERROR=$?
 
-# Guardem la ID del grup per accions posteriors
-ID=$(echo $(aws ec2 describe-security-groups --filter Name=group-name,Values="sg-hackaton" --query 'SecurityGroups[0].[GroupId]' --output text))
+# Comprovem errors
+if [ $CODIERROR -ne 0 ]; then
+	echo "Error: No s'ha pogut crear el grup de seguretat. Codi d'error: $CODIERROR"
+	exit 2
+fi
 
+# Guardem la ID del grup per accions posteriors
+ID= $(aws ec2 describe-security-groups --filter Name=group-name,Values="sg-hackaton" --query 'SecurityGroups[0].[GroupId]' --output text)
+echo "ID del SG: $ID"
+
+# Afegim regles d'entrada
+# HTTP (443)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 80 --cidr 0.0.0.0/0
+# HTTPS (80)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 443 --cidr 0.0.0.0/0
+
+# SSH (22)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+# RDP(3389)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 3389 --cidr 0.0.0.0/0
+
+# DNS (UDP) (53)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol udp --port 53 --cidr 0.0.0.0/0
+# DNS (TCP) (53)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 53 --cidr 0.0.0.0/0
+
+# LDAP (389)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 389 --cidr 0.0.0.0/0
+# Secure LDAP (636)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 636 --cidr 0.0.0.0/0
