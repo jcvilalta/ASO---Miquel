5b9c6080fb872f6b5c0d15ec02abc9ab8f00c79d
jcvilalta <jcastanyervilalta@gmail.com>
2024-11-21 19:38:04 +0100

    update

--- AWSCLI/scripts/crearSG.sh
+++ AWSCLI/scripts/crearSG.sh
@@ -21,19 +21,16 @@ echo "ID del SG: $ID"
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 80 --cidr 0.0.0.0/0
 # HTTPS (80)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 443 --cidr 0.0.0.0/0
-
 # SSH (22)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 22 --cidr 0.0.0.0/0
-
 # RDP(3389)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 3389 --cidr 0.0.0.0/0
-
 # DNS (UDP) (53)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol udp --port 53 --cidr 0.0.0.0/0
 # DNS (TCP) (53)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 53 --cidr 0.0.0.0/0
-
 # LDAP (389)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 389 --cidr 0.0.0.0/0
 # Secure LDAP (636)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 636 --cidr 0.0.0.0/0
+
