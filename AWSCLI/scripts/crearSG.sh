7d68a95c0f6cb19a0bac3970f539525c608d171f
jcvilalta <jcastanyervilalta@gmail.com>
2024-11-22 07:20:40 +0100

    arreglar obtencio ID del sg

--- AWSCLI/scripts/crearSG.sh
+++ AWSCLI/scripts/crearSG.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Creem el grup de seguretat
-aws ec2 create-security-group --group-name sg-hackaton --description "Security group pel Hackaton" > /dev/null 2>&1
+aws ec2 create-security-group --group-name hackaton-sg --description "Security group pel Hackaton"
 
 # Obtenim el codi de sortida
 CODIERROR=$?
@@ -13,7 +13,7 @@ if [ $CODIERROR -ne 0 ]; then
 fi
 
 # Guardem la ID del grup per accions posteriors
-ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values="sg-hackaton" --query 'SecurityGroups[0].[GroupId]' --output text)
+ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values="hackaton-sg" --query 'SecurityGroups[0].[GroupId]' --output text)
 
 # Comprovem si s'ha obtingut la ID
 if [ -z "$ID" ]; then
@@ -24,9 +24,9 @@ fi
 echo "ID del SG: $ID"
 
 # Afegim regles d'entrada
-# HTTP (80
+# HTTP (80)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 80 --cidr 0.0.0.0/0
-# HTTPS (443
+# HTTPS (443)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 443 --cidr 0.0.0.0/0
 # SSH (22)
 aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 22 --cidr 0.0.0.0/0
