1f58ea2d0f553076009da541837b5d044f99ca2e
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-16 10:28:24 -0500

    torno a començar i assigno variables

--- scripts/massaGran/massaGran.sh
+++ scripts/massaGran/massaGran.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# Comprovar si es passen dos arguments
+# Comprovar que es passin dos arguments
 if [ $# -ne 2 ]; then
   echo "Ús: $0 MIDA FITXER"
   exit 1
 fi
 
 # Assignar arguments a variables
-MIDA=$1
-echo "Mida: $MIDA"
-
-FITXER=$2
-echo "Fitxer $FITXER"
-
-# Obtenir la mida del fitxer
-MIDAFITXER=$(du -b "$FITXER" | cut -f1)
-
-# Fem la diferència de la mida que hem posat amb la mida del fitxer
-let DIF=$MIDAFITXER-$MIDA
-echo "Mida del fitxer: $MIDAFITXER"
-echo "Diferència: $DIF"
-
-# Comprovar si el fitxer existeix
-if [ ! -f "$FITXER" ]; then
-  echo "Error: El fitxer '$FITXER' no existeix."
-  exit 1
-fi
-
-# Comparar la mida del fitxer amb la mida donada
-if [ "$MIDAFITXER" -gt "$MIDA" ]; then
-  echo "El fitxer '$FITXER' és més gran que $numMida bytes i és $DIF bytes més gran."
-
-fi
+SIZE=$1
+FILE=$2
+USER=$(whoami)
+DATE_HOUR=$(date "+%Y-%m-%d %H:%M:%S")
+LOGS="/var/log/scriptsErrors/massaGran.log"
