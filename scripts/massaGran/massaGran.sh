2ba148fa85ed7075b801921ad91cbac5c1baeb5f
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-16 10:45:34 -0500

    corregir petits errors

--- scripts/massaGran/massaGran.sh
+++ scripts/massaGran/massaGran.sh
@@ -14,7 +14,7 @@ DATE_HOUR=$(date "+%Y-%m-%d %H:%M:%S")	# Data i hora actuals
 LOGS="/var/log/scriptsErrors/massaGran.log"	# Ruta fitxer de logs
 
 # Comprovar l'existència del fitxer
-if [ ! -f "FILE" ]; then
+if [ ! -f "$FILE" ]; then
 	echo "$DATE_HOUR - Usuari: $USER - ERROR: $FILE no existeix." >>  "$LOGS"
 	exit 1
 fi
@@ -23,7 +23,7 @@ fi
 FILE_SIZE_KB=$(du -k "$FILE" | cut -f1)
 
 # Comprovar la mida del fitxer
-if [$FILE_SIZE_KB -gt $FILE_SIZE ]; then
+if [ $FILE_SIZE_KB -gt $FILE_SIZE ]; then
 	DIFFERENCE=$((FILE_SIZE_KB - FILE_SIZE))
 	echo "$DATE_HOUR - ALERTA: El fitxer $FILE és més gran que $FILE_SIZE kB (Diferència: $DIFFERENCE kB). Usuari: $USER" >> "$LOGS"
 fi
