5fe86cde9745ffe441b1e0100ec0b9c158bc5f09
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-17 08:28:15 -0500

    directoris origen/desti, data i nom fitxer

---
+++ scripts/copiesSeguretat/copiesSeguretat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Dir origen i dir destí
+DIR_ORIGEN="/home/xukim/Documents"
+DIR_DESTI="/home/xukim/copies"
+
+# Data actual (per incloure al nom de la còpia)
+DATA=$(date +'%d-%m-%Y')
+
+# Nom fitxer còpia
+FITXER_COPIA="còpia_$DATA.tar.gz"
