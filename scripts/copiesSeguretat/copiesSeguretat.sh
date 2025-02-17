2221f0c037848f6dfd0d9fd33c28879bb963bf6e
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-17 15:52:25 +0100

    gestio d'errors

--- scripts/copiesSeguretat/copiesSeguretat.sh
+++ scripts/copiesSeguretat/copiesSeguretat.sh
@@ -10,6 +10,9 @@ DATA=$(date +'%d-%m-%Y_%H-%M-%S')
 # Nom fitxer còpia
 FITXER_COPIA="còpia_$DATA.tar.gz"
 
+# Fitxer de logs
+LOGS="/var/log/scriptsErrors/copiesSeguretat/copiesSeguretat.log"
+
 # Demanem confirmació per crear la còpia
 
 read -p "Estàs segur que vols crear una còpia de seguretat? (s/n): " CONFIRMAR
@@ -21,9 +24,15 @@ if [[ "$CONFIRMAR" == "s" || "$CONFIRMAR" == "S" ]]; then
 	# Comprovem si la còpia s'ha creat correctament
 	if [[ -f "$DIR_DESTI/$FITXER_COPIA" ]]; then
 		echo "La còpia de seguretat ha sigut creada amb èxit: $DIR_DESTI/$FITXER_COPIA"
+		# Afegir als logs
+                echo "$(date +'%d-%m-%Y %H:%M:%S') - Còpia creada: $DIR_DESTI/$FITXER_COPIA" >> "$LOGS"
 	else
 		echo "Hi ha hagut un problema en crear la còpia de seguretat."
+		# Afegir als logs
+		echo "$(date +'%d-%m-%Y %H:%M:%S') - ERROR: No s'ha pogut crear la còpia." >> "$LOGS"
 	fi
 else
 	echo "Còpia de seguretat no creada."
+	# Afegir als logs
+	echo "$(date +'%d-%m-%Y %H:%M:%S') - Còpia no creada (cancel·lat per l'usuari)." >> "$LOGS"
 fi
