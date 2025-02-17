c3f1004afd6736c99838e3b08c390613f63d1e0b
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-17 09:21:00 -0500

    demanar confirmacio i donar la sortida per pantalla

--- scripts/copiesSeguretat/copiesSeguretat.sh
+++ scripts/copiesSeguretat/copiesSeguretat.sh
@@ -10,5 +10,20 @@ DATA=$(date +'%d-%m-%Y')
 # Nom fitxer còpia
 FITXER_COPIA="còpia_$DATA.tar.gz"
 
-# Craer còpia compressa
-tar -czf "$DIR_DESTI/$FITXER_COPIA" -C "$DIR_ORIGEN" .
+# Demanem confirmació per crear la còpia
+
+read -p "Estàs segur que vols crear una còpia de seguretat? (s/n): " CONFIRMAR
+
+if [[ "$CONFIRMAR" == "s" || "$CONFIRMAR" == "S" ]]; then
+	# Crear còpia compressa
+	tar -czf "$DIR_DESTI/$FITXER_COPIA" -C "$DIR_ORIGEN" .
+
+	# Comprovem si la còpia s'ha creat correctament
+	if [[ -f "$DIR_DESTI/$FITXER_COPIA" ]]; then
+		echo "La còpia de seguretat ha sigut creada amb èxit: $DIR_DESTI/$FITXER_COPIA"
+	else
+		echo "Hi ha hagut un problema en crear la còpia de seguretat."
+	fi
+else
+	echo "Còpia de seguretat no creada."
+fi
