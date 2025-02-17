c5352a620e22aff5014e8404d9dbb792ca41f846
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-17 08:41:45 -0500

    creació de les còpies

--- scripts/autoCommit/autoCommit.sh
+++ scripts/autoCommit/autoCommit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MISSATGE="$1"
-RUTA="/home/xukim/work/jcvilalta/scripts"
+RUTA="/home/xukim/work/ASO---Miquel/scripts"
 
 git add $RUTA
 git commit -m "$MISSATGE"
--- scripts/copiesSeguretat/copiesSeguretat.sh
+++ scripts/copiesSeguretat/copiesSeguretat.sh
@@ -9,3 +9,6 @@ DATA=$(date +'%d-%m-%Y')
 
 # Nom fitxer còpia
 FITXER_COPIA="còpia_$DATA.tar.gz"
+
+# Craer còpia compressa
+tar -czf "$DIR_DESTI/$FITXER_COPIA" -C "$DIR_ORIGEN" .
