786646af55f5f5db11054cc1b5f90c8b0b852297
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-21 13:46:31 +0100

    variables

--- scripts/copiesSeguretat/copiesSeguretatNou.sh
+++ scripts/copiesSeguretat/copiesSeguretatNou.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
 # copia DIRECTORI [ELIMINAR=N]
+
+# Directori on es guardaran les còpies
+DIR_DESTI="/home/xukim/copies"
+
+# Fitxer de logs
+FITXER_LOG="/var/log/scriptsErrors/copiesSeguretat/copies.log"
+
+# Directori origen (del qual es fa la còpia)
+DIR_ORIGEN="$1"
