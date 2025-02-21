83b42cde1f59045fa3164d92dd3e884001967586
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-21 13:52:30 +0100

    comprovar existència directori desti, crear-lo si no existeix

--- scripts/copiesSeguretat/copiesSeguretatNou.sh
+++ scripts/copiesSeguretat/copiesSeguretatNou.sh
@@ -15,3 +15,13 @@ if [ "$#" -lt 1 ]; then
     echo "Ús: $0 <directori_origen> [ELIMINAR=N]" >> "$FITXER_LOG"
     exit 1
 fi
+
+# Comprovar que el directori origen existeix
+if [ ! -d "$DIR_ORIGEN" ]; then
+	echo "Error: El directori '$DIR_ORIGEN' no existeix. " >> "$FITXEER_LOG"
+	exit 2
+fi
+
+# Crear el directori destí si no existeix
+mkdir -p "$DIR_DESTI"
+
