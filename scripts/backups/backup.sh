ab08ccd5fb7f06e62cfe8bcc8cca7c12bf57f3c1
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-21 14:19:33 +0100

    crear carpeta backups i moure script

--- scripts/backups/backup.sh
+++ scripts/backups/backup.sh
@@ -18,21 +18,21 @@ fi
 
 # Comprovar que el directori origen existeix
 if [ ! -d "$DIR_ORIGEN" ]; then
-	echo "Error: El directori '$DIR_ORIGEN' no existeix. " >> "$FITXEER_LOG"
-	exit 2
+        echo "Error: El directori '$DIR_ORIGEN' no existeix. " >> "$FITXER_LOG"
+        exit 2
 fi
 
 # Crear el directori destí si no existeix
 mkdir -p "$DIR_DESTI"
 
 # Generar nom de l'arxiu de la còpia
-DATA=$(date + %Y&m%d_%H%M%S")
-FITXER_COPIA="$DIR_DESTI/copia_$(basename "$DIR_DESTI")_$DATA.tar.gz"
+DATA=$(date +%Y%m%d_%H%M%S)
+FITXER_COPIA="$DIR_DESTI/copia_$(basename "$DIR_ORIGEN")_$DATA.tar.gz"
 
 # Fer la còpia de seguretat
 if tar -czf "$FITXER_COPIA" -C "$(dirname "$DIR_ORIGEN")" "$(basename "$DIR_ORIGEN")" 2>> "$FITXER_LOG"; then
-	echo "Còpia de seguretat creada: $FITXER_COPIA" >> "$FITXER_LOG"
+        echo "Còpia de seguretat creada: $FITXER_COPIA" >> "$FITXER_LOG"
 else
-	echo "Error en crear la còpia de seguretat" >> "$FITXER_LOG"
-	exit 3
+        echo "Error en crear la còpia de seguretat" >> "$FITXER_LOG"
+        exit 3
 fi
--- scripts/copiesSeguretat/copies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# copia DIRECTORI [ELIMINAR=N]
