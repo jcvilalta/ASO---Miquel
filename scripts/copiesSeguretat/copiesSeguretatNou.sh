5c88ee27a45e328dc3732bebdba057f879f5771e
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-21 13:48:36 +0100

    comprovar que es passa 1 argument

--- scripts/copiesSeguretat/copiesSeguretatNou.sh
+++ scripts/copiesSeguretat/copiesSeguretatNou.sh
@@ -9,3 +9,9 @@ FITXER_LOG="/var/log/scriptsErrors/copiesSeguretat/copies.log"
 
 # Directori origen (del qual es fa la còpia)
 DIR_ORIGEN="$1"
+
+# Comprovar que s'ha passat almenys un argument
+if [ "$#" -lt 1 ]; then
+    echo "Ús: $0 <directori_origen> [ELIMINAR=N]" >> "$FITXER_LOG"
+    exit 1
+fi
