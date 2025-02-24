e1ec38d2267ada80868e7662d391b34f632ed2f0
jcvilalta <jcastanyervilalta@gmail.com>
2025-02-24 18:06:07 +0100

    gestió errors

--- scripts/PID/usCPU.sh
+++ scripts/PID/usCPU.sh
@@ -2,11 +2,24 @@
 
 # Comprovar si s'ha introduid un PID
 if [ -z "$1" ]; then
+	echo "Error: Has d'especificar un PID."
 	echo "Ús: $0 <PID>"
 	exit 1
 fi
 
 PID=$1
+
+# Comprovar si el PID és un número vàlid
+if ! [[ "$PID" =~ ^[0-9]+$ ]]; then
+	echo "Error: El PID ha de ser un número."
+	exit 1
+fi
+
+Comprovar si `top` està instal·lat
+if ! command -v top &> /dev/null; then
+	echo "Error: El comandament 'top' no està instal·lat."
+	exit 1
+fi
 
 # Executar top per obtenir l'ús de la CPU del procés
 US_CPU=$(top -b -n 1 -p $PID | awk 'NR>7 {print $9}')
