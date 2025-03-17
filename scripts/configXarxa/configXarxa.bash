847bbc6f06e9c0a19bbb89a27561d16912d3f36a
jcvilalta <jcastanyervilalta@gmail.com>
2025-03-17 18:28:01 +0100

    verificar usuari root (execució script)

--- scripts/configXarxa/configXarxa.bash
+++ scripts/configXarxa/configXarxa.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verificar que l'usuari és root
+if [ "$(id -u)" != "0" ]: then
+	echo "ERROR: Aquest script s'ha d'executar com a root"
+	exit 1
+fi
+
 # Variables
 BACKUP_DIR="/var/backups/network"
 NETPLAN_DIR="/etc/netplan"
