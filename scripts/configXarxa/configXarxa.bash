af6cc4f9e972d07aad68982f6c9a49ff13fb315f
jcvilalta <jcastanyervilalta@gmail.com>
2025-03-17 18:29:11 +0100

    TIMESTAMP i BACKUP_PATH

--- scripts/configXarxa/configXarxa.bash
+++ scripts/configXarxa/configXarxa.bash
@@ -7,7 +7,9 @@ if [ "$(id -u)" != "0" ]: then
 fi
 
 # Variables
-BACKUP_DIR="/var/backups/network"
+BACKUP_DIR="/var/backups/xarxa"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_PATH="$BACKUP_DIR/$TIMESTAMP"
 NETPLAN_DIR="/etc/netplan"
 RESOLV_CONF="/etc/resolv.conf"
 FIREWALL_PROFILES=("none" "webserver" "ssh-only")
